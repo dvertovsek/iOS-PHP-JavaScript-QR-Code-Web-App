04c3b220c932d15e63790cad19f75060aa2501cf
Darijan <dvertovs@foi.hr>
2016-01-15 14:24:25 +0100

    created iOS project

---
+++ QRScan/QRScan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QRScan
+//
+//  Created by Faculty of Organisation and Informatics on 15/01/16.
+//  Copyright © 2016 dater. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
